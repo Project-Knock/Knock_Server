9567ddb4980e0cb96132313730d7e8c3f9393a22
“bitbyte08” <eight2823@naver.com>
2024-11-21 10:54:10 +0000

    edit : 카메라 불러오기 구현

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../style/components/Camera.css'
-const Camera = () =>{
+const Camera = (loggedIn) =>{
+    const [url,setUrl] = useState(null);
+    const loadCamUrl = () =>{
+        fetch('/myroom/info/cam')
+          .then((res) => res.json())
+          .then((data) => {
+            if(data.url===null){
+              console.log(data);
+            }else{
+              setUrl(data.url)
+            }
+          }
+        );
+      }
+      useEffect(() => { // loggedIn이 바뀔 때 마다 정보 불러오기
+        if(loggedIn) loadCamUrl(); // 현제 세션이 서버에 있을 때 데이터 불러오기
+      },[loggedIn]);
     return(
         <article className='camera-display'>
-            <img src={null}/>
+            <img src={url}/>
         </article>
     )
 }
