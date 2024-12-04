04fe915aaeeca0a706e95ffe903cf4c01ff8a35b
“bitbyte08” <eight2823@naver.com>
2024-12-04 05:55:31 +0000

    edit: 컨트롤러 로그인 여부 추가

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -5,13 +5,24 @@ import Window from '../assets/image/Window.svg';
 import Aircon from '../assets/image/Air conditioner.svg';
 import Door from '../assets/image/Door.svg';
 import BarBtn from './BarBtn';
-const Control = () =>{
+const Control = ({loggedIn}) =>{
     return(
         <nav className='control'>
             <ul className='items'>
-                <BarBtn path='/myroom/window' src={Window} />
-                <BarBtn path='/myroom/aircon/' src={Aircon} />
-                <BarBtn path='/myroom/door' src={Door} />
+                {loggedIn?(
+                    <>
+                        <BarBtn path='/myroom/window' src={Window} />
+                        <BarBtn path='/myroom/aircon/' src={Aircon} />
+                        <BarBtn path='/myroom/door' src={Door} />
+                    </>
+                ):(
+                    <>
+                        <BarBtn path='/user/login' src={Window} />
+                        <BarBtn path='/user/login' src={Aircon} />
+                        <BarBtn path='/user/login' src={Door} />
+                    </>
+                )}
+                
             </ul>
         </nav>
     )
